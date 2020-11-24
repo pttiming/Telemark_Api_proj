9a9acad3bebd7f120b53b76c5b74aa7f4bfca34a
pttiming <sean@pttiming.com>
2020-11-24 16:52:39 -0600

    Initial Commit of API

---
+++ WebAPISample/Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPISample.Models;
+
+namespace WebAPISample.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movie>()
+             .HasData(
+                new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" },
+                new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" },
+                new Movie { MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" }
+             );
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
